f114660876dc2f744be7a9a6bdad01643f4b66f7
Alexey Popov <alexey.popov@umbrella-web.com>
2016-08-03 14:02:28 +0300

    add state to Article component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -10,14 +10,29 @@ const propTypes = {
 };
 
 class Article extends React.Component {
+    constructor() {
+        super();
+        this.readmoreClick = this.readmoreClick.bind(this);
+        this.state = { visible: false };
+    }
+
+    readmoreClick(e) {
+        e.preventDefault();
+        this.setState({ visible: true });
+    }
+
     render() {
         const author = this.props.data.author;
         const text = this.props.data.text;
         const bigText = this.props.data.bigText;
+        const visibleClass = this.state.visible ? 'none' : '';
+
         return (
             <div className="article">
                 <p className="news__author">{author}:</p>
                 <p className="news__text">{text}</p>
+                <button onClick={this.readmoreClick} className={`news__readmore ${visibleClass}`}>Подробнее
+                </button>
                 <p className="news__big-text">{bigText}</p>
             </div>
         );
